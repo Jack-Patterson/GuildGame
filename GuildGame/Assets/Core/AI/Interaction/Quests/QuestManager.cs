2540c90874229c832ae1445d0937db9ccdb513ca
Jack-Patterson <jackpatterson.10.02@gmail.com>
2024-05-14 17:50:14 +0100

    Weighted quest

--- GuildGame/Assets/Core/AI/Interaction/Quests/QuestManager.cs
+++ GuildGame/Assets/Core/AI/Interaction/Quests/QuestManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using com.Halkyon.AI.Character.Attributes;
 using UnityEngine;
 
 namespace com.Halkyon.AI.Interaction.Quests
@@ -33,14 +34,54 @@ namespace com.Halkyon.AI.Interaction.Quests
             if (_quests.Count == 0)
             {
                 int questsToGet = Random.Range(2, 5);
+                System.Random random = new System.Random();
+
                 for (int i = 0; i < questsToGet; i++)
                 {
-                    Quest quest = Quests[Random.Range(0, Quests.Count)];
+                    Quest quest = GetRandomWeightedQuest(random);
                     quests.Add(quest);
                 }
             }
-            
+
             return quests;
+        }
+        
+        private static Quest GetRandomWeightedQuest(System.Random random)
+        {
+            int totalWeight = 0;
+            List<int> cumulativeWeights = new List<int>();
+
+            foreach (var quest in Quests)
+            {
+                totalWeight += GetWeightForRank(quest.requiredRank);
+                cumulativeWeights.Add(totalWeight);
+            }
+
+            int randomValue = random.Next(totalWeight);
+            for (int i = 0; i < cumulativeWeights.Count; i++)
+            {
+                if (randomValue < cumulativeWeights[i])
+                {
+                    return Quests[i];
+                }
+            }
+
+            return Quests[^1];
+        }
+
+        private static int GetWeightForRank(CharacterRank rank)
+        {
+            switch (rank)
+            {
+                case CharacterRank.F: return 50;
+                case CharacterRank.E: return 30;
+                case CharacterRank.D: return 20;
+                case CharacterRank.C: return 10;
+                case CharacterRank.B: return 5;
+                case CharacterRank.A: return 2;
+                case CharacterRank.S: return 1;
+                default: return 1;
+            }
         }
     }
 }
